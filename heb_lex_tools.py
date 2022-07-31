984489dfcc375d759294e68a33426789e7211c5f
Charles Loder <cloder@classicalsubjects.com>
2022-07-31 18:48:09 -0400

    Update return if not found

--- heb_lex_tools.py
+++ heb_lex_tools.py
@@ -13,31 +13,31 @@ class HEBLEX():
                 self.data[form] = gloss
                 self.strongs[strong] = gloss
                 self.lemmas[strong] = form
-    
-    
+
+
     def strongs_to_gloss(self,i):
         if i in self.strongs:
             return self.strongs[i]
         else:
             print(f"{i} not in Strongs->gloss data", file=sys.stderr)
-            return '??'
-            
-            
+            return f"no gloss for {i}"
+
+
     def lemma_to_gloss(self,i):
         if i in self.data:
             return self.data[i]
         else:
             print(f"{i} not in lemma->gloss data", file=sys.stderr)
-            return '??'
-            
-            
+            return f"no gloss for {i}"
+
+
     def strongs_to_lemma(self,i):
         if i in self.lemmas:
             return self.lemmas[i]
         else:
             print(f"{i} not in Strongs->lemma data", file=sys.stderr)
-            return '??'
-            
-    
+            return f"no lemma for {i}"
+
+
 
 
